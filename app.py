9ab46851326fa051d1d3b0573452fb54185d236e
Raphael Salazar <127420583+pngRMS@users.noreply.github.com>
2024-10-06 21:42:31 +0800

    changed def route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import requests
 
 app = Flask(__name__)
@@ -8,31 +8,33 @@ IP_API_URL = "https://ipapi.co/json/"
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    # Redirect to the /get_ip_info route
+    return redirect(url_for('get_ip_info'))
 
-@app.route('/get_ip_info', methods=['POST'])
+@app.route('/get_ip_info', methods=['POST', 'GET'])
 def get_ip_info():
-    # Send a GET request to fetch the IP info
-    response = requests.get(IP_API_URL).json()
+    # If the method is GET, fetch the IP info
+    if request.method == 'GET':
+        response = requests.get(IP_API_URL).json()
 
-    # Check if the response contains required data
-    if response:
-        ip_info = {
-            'ip': response.get('ip'),
-            'version': response.get('version'),
-            'city': response.get('city'),
-            'region': response.get('region'),
-            'country': response.get('country_name'),
-            'country_code': response.get('country_code'),
-            'latitude': response.get('latitude'),
-            'longitude': response.get('longitude'),
-            'asn': response.get('asn'),
-            'org': response.get('org'),
-        }
-        return render_template('index.html', ip_info=ip_info)
-    else:
-        error_message = "Could not retrieve IP information."
-        return render_template('index.html', error=error_message)
+        # Check if the response contains required data
+        if response:
+            ip_info = {
+                'ip': response.get('ip'),
+                'version': response.get('version'),
+                'city': response.get('city'),
+                'region': response.get('region'),
+                'country': response.get('country_name'),
+                'country_code': response.get('country_code'),
+                'latitude': response.get('latitude'),
+                'longitude': response.get('longitude'),
+                'asn': response.get('asn'),
+                'org': response.get('org'),
+            }
+            return render_template('index.html', ip_info=ip_info)
+        else:
+            error_message = "Could not retrieve IP information."
+            return render_template('index.html', error=error_message)
 
 if __name__ == '__main__':
     app.run(debug=True)
